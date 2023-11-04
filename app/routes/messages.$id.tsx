247e5cf5891c96a0209199ad240637e15ca9905e
Kobe Ruado <insidiae423@gmail.com>
2023-11-05 00:05:20 +0800

    Add Optimistic UI example

--- app/routes/messages.$id.tsx
+++ app/routes/messages.$id.tsx
@@ -1,20 +1,49 @@
-import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import {
+	ActionFunctionArgs,
+	LoaderFunctionArgs,
+	defer,
+	json,
+} from "@remix-run/node";
+import { useLoaderData, useNavigation } from "@remix-run/react";
+import MessageDisplay from "~/components/MessageDisplay";
 import { db } from "~/utils/db.server";
 
+// export async function loader({ params }: LoaderFunctionArgs) {
+// 	return json({
+// 		message: await db.message.findUnique({
+// 			where: {
+// 				id: params.id,
+// 			},
+// 			include: {
+// 				replies: {
+// 					select: { id: true, author: true, content: true },
+// 					orderBy: { createdAt: "desc" },
+// 				},
+// 			},
+// 		}),
+// 	});
+// }
+
 export async function loader({ params }: LoaderFunctionArgs) {
-	return json({
-		message: await db.message.findUnique({
-			where: {
-				id: params.id,
-			},
-			include: {
-				replies: {
-					select: { id: true, author: true, content: true },
-					orderBy: { createdAt: "desc" },
-				},
-			},
+	const message = await db.message.findUnique({
+		where: {
+			id: params.id,
+		},
+		select: { author: true, content: true },
+	});
+	const repliesPromise = new Promise((resolve, reject) => {
+		setTimeout(resolve, 3000);
+	}).then(() =>
+		db.message.findMany({
+			where: { parent_id: params.id },
+			select: { id: true, author: true, content: true },
+			orderBy: { createdAt: "desc" },
 		}),
+	);
+
+	return defer({
+		message,
+		replies: repliesPromise,
 	});
 }
 
@@ -41,69 +70,23 @@ export async function action({ request, params }: ActionFunctionArgs) {
 }
 
 export default function Index() {
-	const data = useLoaderData<typeof loader>();
+	const { message, replies } = useLoaderData<typeof loader>();
+	const navigation = useNavigation();
 
-	return (
-		<div className="flex flex-col gap-8 p-8">
-			<Link to="/">
-				<h1 className="text-3xl font-bold">&larr; Message</h1>
-			</Link>
+	if (navigation.formData) {
+		const author = navigation.formData.get("author") || "Anonymous";
+		const content = navigation.formData.get("content");
 
-			<article
-				key={data.message?.id}
-				className="flex flex-col rounded-[1.375rem] border border-black p-4"
-			>
-				<p>
-					<span className="font-bold">{data.message?.author}</span> posted:
-				</p>
-				<p className="mb-4">{data.message?.content}</p>
+		if (typeof author === "string" && typeof content === "string") {
+			return (
+				<MessageDisplay
+					message={message}
+					replies={replies}
+					pendingReply={{ author, content }}
+				/>
+			);
+		}
+	}
 
-				<section className="flex flex-col gap-4">
-					<h2 className="text-xl font-bold">Replies</h2>
-
-					<form method="post" className="flex flex-col gap-2">
-						<label htmlFor="author" className="text-lg font-medium">
-							Screen Name
-						</label>
-						<input
-							type="text"
-							name="author"
-							id="author"
-							placeholder="Anonymous"
-							className="mb-2 border border-black p-2"
-						/>
-						<label htmlFor="content" className="text-lg font-medium">
-							Message
-						</label>
-						<textarea
-							name="content"
-							id="content"
-							className="mb-2 border border-black p-2"
-							placeholder="Your Message"
-							required
-						></textarea>
-						<button className="rounded-full bg-blue-600 py-2 text-center text-white">
-							Post Reply
-						</button>
-					</form>
-
-					{data.message?.replies.length !== 0 ? (
-						<div className="flex flex-col gap-4">
-							{data.message?.replies.map((reply) => (
-								<article
-									key={reply.id}
-									className="flex flex-col rounded-md border border-black p-4"
-								>
-									<p>
-										<span className="font-bold">{reply.author}</span> replied:
-									</p>
-									<p>{reply.content}</p>
-								</article>
-							))}
-						</div>
-					) : null}
-				</section>
-			</article>
-		</div>
-	);
+	return <MessageDisplay message={message} replies={replies} />;
 }
